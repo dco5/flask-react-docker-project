3fb6b45f58ed0c7066ed1c78d6114735b971ffff
jlossada <jlosada87@hotmail.com>
2019-07-19 21:12:36 -0500

    - added migrations - added unique constrains to username and email

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -3,11 +3,13 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 toolbar = DebugToolbarExtension()
 cors = CORS()
+migrate = Migrate()
 
 
 def create_app(script_info=None):
@@ -22,6 +24,7 @@ def create_app(script_info=None):
     db.init_app(app)
     toolbar.init_app(app)
     cors.init_app(app)
+    migrate.init_app(app, db)
 
     # register blueprints
     from project.api.users import users_blueprint
